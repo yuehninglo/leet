b7d52cbee068623dae8884c2b2bdb9dcc4d872e2
yn <hisa905395@gmail.com>
2026-01-03 15:30:31 +0800

    https://leetcode.com/problems/powx-n/description/ (Time Limit Exceeded)

--- src/Leet.java
+++ src/Leet.java
@@ -1,6 +1,35 @@
+import java.math.BigDecimal;
+
 public class Leet {
     static void main() {
-        System.out.println("first");
-    }
+		var leet = new Leet();
+		leet.myPow(2, 10);
+	}
+
+	public double myPow(double x, int n) {
+		if (n == 1) {
+			return x;
+		} else if (n == -1) {
+			return 1/x;
+		} else {
+			int asbN = Math.abs(n);
+			BigDecimal bx = new BigDecimal(x);
+			BigDecimal res = new BigDecimal(1);
+			for (int i = 0; i < asbN; i++) {
+				res = res.multiply(bx);
+				System.out.println("res: " + res);
+			}
+			if (n > 0) {
+				return Double.valueOf(res.toString());
+			} else {
+				BigDecimal one = new BigDecimal(1);
+				return Double.valueOf(one.divide(res).toString());
+			}
+		}	
+    }	
+
+
 }
+
+
 
