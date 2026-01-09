48862bbb6f3193b14853adb25a41ac0f336856dd
yn <hisa905395@gmail.com>
2026-01-10 00:51:09 +0800

    reformat

--- Leet.java
+++ Leet.java
@@ -113,12 +113,12 @@ public class Leet {
 	
 	// https://leetcode.com/problems/gray-code/
 	public List<Integer> grayCode(int n) {
-		List<Integer> rs=new ArrayList<Integer>();
+		List<Integer> rs = new ArrayList<Integer>();
 		rs.add(0);
-		for(int i=0;i<n;i++){
-			int size=rs.size();
-			for(int k=size-1;k>=0;k--)
-				rs.add(rs.get(k) | 1<<i);
+		for(int i = 0; i < n; i++){
+			int size = rs.size();
+			for(int k = size-1; k >= 0; k--)
+				rs.add(rs.get(k) | 1 << i);
 		}
 		return rs;
 	}
