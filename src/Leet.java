b471258ad47bf6e0f2b93f389b1d81e910c8388c
yn <hisa905395@gmail.com>
2026-01-20 13:41:40 +0800

    populating-next-right-pointers-in-each-node-ii solved

--- src/Leet.java
+++ src/Leet.java
@@ -202,31 +202,36 @@ public class Leet {
     // https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
     public Node connect(Node root) {
         if(root != null) {
-            Node res = new Node();
-            Node current = res;
-            Queue<Node> q = new ArrayDeque<>();
+            Queue<Node> q = new LinkedList<>();
             q.add(root);
-            while(q.size() > 0) {
+            while(!q.isEmpty()) {
                 int len = q.size();
                 for (int i = 0; i < len; i++) {
-                    Node n = q.remove();
-                    current.next = n;
-                    current = current.next;
-                    if(n != null) {
-                        if (n.left != null) q.add(n.left);
-                        if (n.right != null) q.add(n.right);
-                    }
+                    Node n = q.poll();
+                    if(i != len-1) n.next = q.peek();
+                    if(n.left != null) q.add(n.left);
+                    if(n.right != null) q.add(n.right);
                 }
-                current.next = new Node();
-                current = current.next;
             }
-            return res.next;
         }
         return root;
     }
 
-
-	
+    public Node connect2(Node root) {
+        if (root == null) return root;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int level = q.size();
+            while (level-- > 0) {
+                Node curr = q.poll();
+                if (level != 0) curr.next = q.peek();
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+            }
+        }
+        return root;
+    }
 }
 
 class Node {
