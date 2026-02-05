914e59837ca3571b1ecec00ded84a84d33c29da9
yn <hisa905395@gmail.com>
2026-02-05 16:31:22 +0800

    larrys-array

--- src/Leet.java
+++ src/Leet.java
@@ -341,10 +341,20 @@ public class Leet {
 	}
 
 	// https://www.hackerrank.com/challenges/larrys-array/problem?isFullScreen=true
+    // The key insight is permutation parity: each 3-element rotation is a cyclic permutation (3-cycle), 
+	// which is an even permutation. 
+	// Therefore, a permutation can be sorted only if it has even parity (even number of inversions).
+    // The simplest solution is to count inversions
 	public static String larrysArray(List<Integer> A) {
-		
-
-		return "";
+		int inversions = 0;
+		for (int i = 0; i < A.size(); i++) {
+			for (int j = i + 1; j < A.size(); j++) {
+				if (A.get(i) > A.get(j)) {
+					inversions++;
+				}
+			}
+		}
+		return inversions % 2 == 0 ? "YES" : "NO";
     }
 
 	// https://leetcode.com/problems/maximum-gap/description/
